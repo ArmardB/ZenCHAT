cc21a31a0f19f0f253365154aefb334c0cc38b7f
Armard Bellamy <armardbellamy@Armard-ZCW3.local>
2016-11-11 17:49:54 -0500

    Updated route to handle 404 errors

--- app/routes/index.js
+++ app/routes/index.js
@@ -17,6 +17,9 @@ module.exports = () => {
         },
         'post': {
             
+        },
+        'NA': (req, res, next) => {
+            res.status(404).sendFile(process.cwd() + '/views/404.htm');
         }
     }
 
