f0761f1c068fff77c09d86551e7d8500a0a2dd05
Armard Bellamy <armardbellamy@Armard-ZCW3.local>
2016-11-11 16:52:38 -0500

    Updated routes for static assets

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
 'use strict';
-
 const express = require('express');
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res, next) => {
+    // res.send('<h1>Hello Express!</h1>');
+    res.render('login');
+});
+
+app.listen(app.get('port'), () => {
+    console.log('ZenCHAT running on Port: ', 3000);
+});
