dfb820fd42747e90fc8bba2486d2c599f01bc8e1
Armard Bellamy <armardbellamy@Armard-ZCW3.local>
2016-11-12 08:18:49 -0500

    Added module for Mongoose

---
+++ app/db/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const config = require('../config');
+const Mongoose = require('mongoose').connect(config.dbURI);
+
+// Log an error if the connection fails
+Mongoose.connection.on('error', error => {
+    console.log("MongoDB Error: ", error);
+});
