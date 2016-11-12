a81e87b5a0476daa43fe33e1cb384452fe8e3576
Armard Bellamy <armardbellamy@Armard-ZCW3.local>
2016-11-12 08:32:06 -0500

    Completed session manager module

--- app/db/index.js
+++ app/db/index.js
@@ -7,3 +7,7 @@ const Mongoose = require('mongoose').connect(config.dbURI);
 Mongoose.connection.on('error', error => {
     console.log("MongoDB Error: ", error);
 });
+
+module.exports = {
+    Mongoose 
+}
